062c293bb9c8cd7561207781f7faeed2d8fee9fb
Rafael <raffbrito@gmail.com>
2024-09-29 20:47:17 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+from flask_smorest import Api
+
+from resources.item import blp as ItemBluePrint
+from resources.store import blp as StoreBluePrint
+
+app = Flask(__name__)
+
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["API_TITLE"] = "Stores REST API"
+app.config["API_VERSION"] = "V1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+
+api = Api(app)
+
+api.register_blueprint(ItemBluePrint)
+api.register_blueprint(StoreBluePrint)
+
+
+## review Chapter 6 : Store Data in DQL Database with SQLAlchemy
